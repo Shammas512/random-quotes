a6bff82412842ef3810d7768fe6f13ea09e8a817
Shammas512 <shammasasif7@example.com>
2025-01-09 17:42:51 +0500

    just check

---
+++ lib/check.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Checkd extends StatelessWidget {
+  const Checkd({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Heeloooo") ,
+      ),
+    );
+  }
+}
